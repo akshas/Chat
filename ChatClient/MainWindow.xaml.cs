b8dc443d96028c712b5d36caccc0b7abb783b501
Александр Кочетов <allexxx81@gmail.com>
2020-09-20 23:57:23 +0200

    Chat 0.1

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _Ip;
+        public string Ip
+        {
+            get
+            {
+                return _Ip;
+            }
+            set
+            {
+                _Ip = value;
+            }
+        }
+        private string _Port;
+
+        public string SPort
+        {
+            get
+            {
+                return _Port;
+            }
+            set
+            {
+                _Port = value;
+            }
+        }
+
+        private string _Nick;
+        public string Nick
+        {
+            get
+            {
+                return _Nick;
+            }
+            set
+            {
+                _Nick = value;
+            }
+        }
+
+
+        private string _Chat;
+        public string Chat
+        {
+            get
+            {
+                return _Chat;
+            }
+            set
+            {
+                _Chat = value;
+            }
+        }
+
+        private string _Message;
+        public string Message
+        {
+            get
+            {
+                return _Message;
+            }
+            set
+            {
+                _Message = value;
+            }
+        }
+
+        TcpClient client;
+        StreamReader sr;
+        StreamWriter sw;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Ip = "127.0.0.1";
+            SPort = "5000";
+            Nick = "Alex";
+
+            Task.Run(() => { 
+            
+                while (true)
+                {
+                    try
+                    {
+                        if (client?.Connected == true)
+                        {
+                            string line = sr.ReadLine();
+                            Chat += line + "\n";
+                            MessageBox.Show(Chat);
+                        }
+                        Task.Delay(10).Wait();
+                    }
+                    catch
+                    {
+
+                    }
+
+                }
+            });
+
+        }
+
+        private void Connect(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+             
+                    try
+                    {
+      
+                        client = new TcpClient();
+                        Int32.TryParse(SPort, out int Port);
+                        client.Connect(Ip, Port);
+                        sr = new StreamReader(client.GetStream());
+                        sw = new StreamWriter(client.GetStream());
+                        sw.AutoFlush = true;
+                        sw.WriteLine($"Login: {Nick}");
+                    }
+                    catch { }
+            });
+        }
+
+        private void Send(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() => {
+                try
+                {
+                    sw.WriteLine($"{Nick}: {Message}");
+                }
+                catch { }
+            });
+        }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static IPAddress IP = IPAddress.Parse("127.0.0.1");
+        static TcpListener listener = new TcpListener(IP, 5000);
+        static List<ConnectedClient> clients = new List<ConnectedClient>();
+
+        static void Main(string[] args)
+        {
+            listener.Start();
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Task.Run(() => 
+                {
+                    StreamReader sr = new StreamReader(client.GetStream());
+                    while (client.Connected)
+                    {
+                        string line = sr.ReadLine();
+                        if (line.Contains("Login: ") && !string.IsNullOrWhiteSpace(line.Replace("Login: ", "")))
+                        {
+                            string nick = line.Replace("Login: ","");
+                            if (clients.FirstOrDefault(connectedClient => connectedClient.Name == nick) == null)
+                            {
+                                clients.Add(new ConnectedClient(client, nick));
+                                Console.WriteLine("New connection: " + nick);
+                                break;
+                            }
+                            else
+                            {
+                                StreamWriter sw = new StreamWriter(client.GetStream());
+                                sw.AutoFlush = true;
+                                sw.WriteLine($"Es gibt schon ein Benutzern min dem Namen [{nick}]");
+                                client.Client.Disconnect(false);
+                            }
+                        }
+                    }
+
+                    while (client.Connected)
+                    {
+                        try
+                        {
+                            sr = new StreamReader(client.GetStream());
+                            string line = sr.ReadLine();
+                            SendToAllClients(line);
+                            Console.WriteLine(line);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                });
+            }
+        }
+
+        private static async void SendToAllClients(string message)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < clients.Count; i++)
+                {
+                    try
+                    {
+                        StreamWriter sw = new StreamWriter(clients[i].Client.GetStream());
+                        sw.AutoFlush = true;
+                        sw.WriteLine(message);
+                    }
+                    catch { }
+                } 
+            });
+        }
+    }
+}
